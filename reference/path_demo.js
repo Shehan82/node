bea3bc9f9a405ca1faf57be779c79d7ce17da427
shehan82 <shehansandeepa82@gmail.com>
2020-11-29 20:16:56 +0530

    added some path functions

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -1,3 +1,18 @@
 const path = require('path');
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
+
+
+console.log(path.basename(__filename));
+
+console.log(path.dirname(__filename));
+
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename)); // for get path as the object
+
+console.log(path.parse(__filename).base);
+
+console.log(path.join(__dirname, "hello", "my.html"));
+
+
