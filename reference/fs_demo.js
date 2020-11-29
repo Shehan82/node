d542c89f25f8718dc36019a971deec34e6e04f46
shehan82 <shehansandeepa82@gmail.com>
2020-11-29 22:08:01 +0530

    write in the file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,7 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 //make folder
-fs.mkdir(path.join(__dirname,'/hello'),(err) => {
+// fs.mkdir(path.join(__dirname,'/hello'),(err) => {
+//     if(err) throw err;
+//     console.log("folder created !");
+// })
+
+//create and write to file
+
+fs.writeFile(path.join(__dirname, '/hello', 'hi.txt'), 'hello i am hello how are you hi', (err) => {
     if(err) throw err;
-    console.log("folder created !");
+    console.log("written in file");
 })
