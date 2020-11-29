115979a35d7cb5f47009eed1b9b1c079a171b1e8
shehan82 <shehansandeepa82@gmail.com>
2020-11-29 22:27:53 +0530

    read a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -9,13 +9,18 @@ const path = require('path');
 
 //create and write to file
 
-fs.writeFile(path.join(__dirname, '/hello', 'hi.txt'), 'hello i am hello how are you hi', (err) => {
-    if(err) throw err;
-    console.log("written in file");
+// fs.writeFile(path.join(__dirname, '/hello', 'hi.txt'), 'hello i am hello how are you hi', (err) => {
+//     if(err) throw err;
+//     console.log("written in file");
 
-    fs.appendFile(path.join(__dirname, '/hello', 'hi.txt'), 'hello i am hello how are you hiafkasfjak121 i am appended one', (err) => {
-        if(err) throw err;
-        console.log("written in file");
-    }) 
-}) 
+//     fs.appendFile(path.join(__dirname, '/hello', 'hi.txt'), 'hello i am hello how are you hiafkasfjak121 i am appended one', (err) => {
+//         if(err) throw err;
+//         console.log("written in file");
+//     }) 
+// }) 
+
+fs.readFile(path.join(__dirname, '/hello', 'hello.txt'), 'utf8', (err, data) => {
+    if(err) throw err;
+    console.log(data);
+})
 
