ba53ba176fd7577c259cc4604bcf3f5fa44dd6d9
shehan82 <shehansandeepa82@gmail.com>
2020-11-30 13:49:48 +0530

    use env variable

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const Student = require('./student');
+// const Student = require('./student');
 
-let student1 = new Student("shehan", "jf");
-student1.details();   
+// let student1 = new Student("shehan", "jf");
+// student1.details();  
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+     res.write("hello how are you!");
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=>{console.log(`Server running on port ${PORT}....`)});
